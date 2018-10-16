32d60b16f1434ad744adc8de2b5aee50ad66c1f5
Damian Otto <otto.damian0@gmail.com>
2018-10-16 22:20:16 +0200

    add new value C1 - circularity

--- window.py
+++ window.py
@@ -1,12 +1,15 @@
-from Tkinter import *
+from tkinter import *
 from PIL import Image, ImageTk
 import SimpleITK
 import matplotlib.pyplot as plt
+from skimage.filters import threshold_mean
+import numpy as np
+import math
 
 filenameT1 = "VSD.Brain.XX.O.MR_Flair.36607.mha"
 filenameT2 = "VSD.Brain.XX.O.MR_T1.36608.mha"
-
-idxSlice = 20;
+value = 0
+idxSlice = 20
 labelGrayMatter = 1
 
 def increaseSlice():
@@ -24,15 +27,21 @@ def decreaseSlice():
 
 def sitk_show(img, title=None, margin=0.0, dpi=40):
     nda = SimpleITK.GetArrayFromImage(img)
+    thresh = threshold_mean(nda)
+    binary = nda > thresh
+    count = np.array(binary);
+    a = np.count_nonzero(count == True)
+    value = 2*math.sqrt(a/math.pi)
+    labelFirstValue.configure(text = value)
     plt.imsave('aaa.jpeg', nda, cmap=plt.cm.gray)
     img2 = ImageTk.PhotoImage(Image.open('aaa.jpeg'))
     label.configure(image=img2)
     label.image = img2
-    #fig = plt.figure()
-    #ax = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
+    fig = plt.figure()
+    ax = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
     #plt.set_cmap("gray")
-    #ax.imshow(nda, interpolation=None)
-    #plt.show()
+    ax.imshow(binary, interpolation=None)
+    plt.show()
 
 def showImg():
     imgT1Original = SimpleITK.ReadImage(filenameT1)
@@ -54,6 +63,9 @@ label.pack(side="bottom", fill="both", expand="yes")
 
 labelSlice = Label(topFrame,text = "Slice number:" + str(idxSlice))
 labelSlice.pack()
+
+labelFirstValue = Label(topFrame, text=str(value))
+labelFirstValue.pack()
 
 button = Button(topFrame, text="ShowIm",command = showImg)
 buttonInc = Button(topFrame, text="Next slice",command = increaseSlice)
